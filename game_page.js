07a23879a10be805607568f1c148ec895b668062
joelcoder2011 <80383585+joelcoder2011@users.noreply.github.com>
2021-12-03 13:33:49 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -12,8 +12,8 @@ document.getElementById("player2_name").innerHTML = player2_name +":";
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
 
-document.getElementById("player1_question").innerHTML = "Question turn - "+ player1_name;
-document.getElementById("player2_answer").innerHTML = "Answer turn - "+ player2_name;
+document.getElementById("player_question").innerHTML = "Question turn - "+ player1_name;
+document.getElementById("player_answer").innerHTML = "Answer turn - "+ player2_name;
 
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
--- main.js
+++ main.js
@@ -5,5 +5,5 @@ function adduser(){
     localStorage.setItem("player1_name",player1_name);
     localStorage.setItem("player2_name",player2_name);
 
-    window.location ="login.html";
+    window.location ="game_page.html";
 }
