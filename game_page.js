62bc34d0ccb6ae152f533a874cd35278dcd25fd1
joelcoder2011 <80383585+joelcoder2011@users.noreply.github.com>
2021-12-06 13:46:45 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -44,28 +44,4 @@ function send(){
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
- question_turn = "player1";
- answer_turn = "player2";
-
- function check()
- {
-     get_answer = document.getElementById("input_check_box").value;
-     if(get_answer == actual_answer){
-         update_player1_score = player1_score + 1;
-         document.getElementById("player1_score").innerHTML = update_player1_score
-     }
-     else
-     {
-        update_player2_score = player2_score + 1;
-        document.getElementById("player2_score").innerHTML = update_player2_score
-     } 
-    if (question_turn = "player2"){
-        question_turn = "player2"
-        document.getElementById("player_question").innerHTML = "Question turn - " + player2_name;  
-    }
-    else{
-        question_turn = "player1"
-        document.getElementById("player_question").innerHTML = "Question turn - " + player1_name;
-    }
-    }
-
+ 
